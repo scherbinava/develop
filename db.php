94e2891d7205308ea07133912a7de196568d0a95
scherbinava <96632679+scherbinava@users.noreply.github.com>
2022-01-07 19:42:07 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","06022003","register");
+$con = mysqli_connect("localhost","root","06022004","register");
 // Check connection
 if (mysqli_connect_errno())
   {
